58aa3358f8addb0af0685667bd4929b8fbfe3d5d
bischoff_m <bischoff_m@aim.com>
2021-03-28 00:04:19 +0100

    add vanilla ReorderableListView and Dismissible items

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'dart:developer' as dev; // TODO: remove in production
@@ -35,6 +36,10 @@ class _HomePageState extends State<HomePage> {
   // style constants TODO: what is best practice to store layout constants?
   final double marginList = 20;
   final double maxWidthMainList = 600;
+  // TODO: use this to set color scheme
+  // final ColorScheme colorScheme = Theme.of(context).colorScheme;
+
+  List<int> _items = List<int>.generate(20, (index) => index);
 
   double getTopMargin(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
@@ -68,6 +73,7 @@ class _HomePageState extends State<HomePage> {
                         focusNode: _textInputFocusNode,
                         autofocus: true,
                         onSubmitted: (itemName) {
+                          // TODO: implement add function
                           dev.log(itemName);
                           _textInputController.clear();
                           _textInputFocusNode.requestFocus();
@@ -75,11 +81,12 @@ class _HomePageState extends State<HomePage> {
                         decoration: InputDecoration(
                           hintText: 'Add Item',
                           border: InputBorder.none,
-                          contentPadding: EdgeInsets.fromLTRB(0, 20, 0, 15),
+                          contentPadding: EdgeInsets.fromLTRB(0, 18, 0, 12),
                           prefixIcon: SizedBox(
                             width: 50,
                             height: 50,
                             child: IconButton(
+                              // TODO: implement add function
                               onPressed: _textInputController.clear,
                               icon: Icon(Icons.add),
                             ),
@@ -101,6 +108,59 @@ class _HomePageState extends State<HomePage> {
                   height: 1,
                   thickness: 1,
                   color: Colors.black,
+                ),
+                Expanded(
+                  child: ReorderableListView(
+                    // TODO: set font size
+                    buildDefaultDragHandles: false,
+                    dragStartBehavior: DragStartBehavior.down,
+                    children: new Iterable<int>.generate(10).map((index) {
+                      if (index == 5) {
+                        return Divider(
+                          key: ValueKey<int>(_items[index]),
+                          height: 128,
+                          thickness: 128,
+                          color: Colors.black,
+                        );
+                      } else {
+                        return Dismissible(
+                          background: Container(
+                            color: Colors.green,
+                          ),
+                          key: ValueKey<int>(_items[index]),
+                          onDismissed: (DismissDirection direction) {
+                            setState(() {
+                              _items.remove(index);
+                            });
+                          },
+                          child: Container(
+                            child: Row(
+                              children: <Widget>[
+                                SizedBox(
+                                  width: 64,
+                                  height: 64,
+                                  child: ReorderableDragStartListener(
+                                    index: index,
+                                    child: Icon(Icons.drag_handle, size: 30),
+                                  ),
+                                ),
+                                Text('Item ${_items[index]}'),
+                              ],
+                            ),
+                          ),
+                        );
+                      }
+                    }).toList(),
+                    onReorder: (int oldIndex, int newIndex) {
+                      setState(() {
+                        if (oldIndex < newIndex) {
+                          newIndex -= 1;
+                        }
+                        final int item = _items.removeAt(oldIndex);
+                        _items.insert(newIndex, item);
+                      });
+                    },
+                  ),
                 ),
               ],
             ),
